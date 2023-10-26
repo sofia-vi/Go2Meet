9253e6f88af506d103ac2f4b4f5b0ddf4ade4889
Alfonso <alfito314@gmail.com>
2023-10-26 17:09:05 +0200

    Fixed duplicate libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,3 +44,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
+
+configurations.implementation {     exclude("org.jetbrains.kotlin", "kotlin-stdlib-jdk8") }
