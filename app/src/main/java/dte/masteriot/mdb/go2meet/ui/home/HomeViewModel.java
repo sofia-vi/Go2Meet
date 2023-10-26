e2cfee666b19efd2fec998ad5d337a3c36f26a7a
sofia <sofia.vinan@tum.de>
2023-10-26 16:31:48 +0200

    Initial commit

---
+++ app/src/main/java/dte/masteriot/mdb/go2meet/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package dte.masteriot.mdb.go2meet.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
